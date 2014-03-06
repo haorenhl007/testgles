b24d4eeb22ea2a343608d0d460e36e3a222b2380
jmpc <jmpc@rain>
2014-03-06 17:18:30 -0200

    Simplify demo file

--- emptySDL.cc
+++ emptySDL.cc
@@ -1,70 +1,53 @@
-#include <iostream>
+#include <stdio.h> 
+#include <assert.h> 
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_opengles2.h>
+#include <SDL.h> 
+#include <SDL_opengles2.h> 
 
-
-const int win_width = 640;
-const int win_height = 480;
-
-// hack due problems with Visual Studio compilation
+// hack for Visual Studio
 #undef main
 
+int main(int _nArg, char *_aArg[]) 
+{ 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES); 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2); 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0); 
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); 
 
-int main(int argc, char *argv[])
-{
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cerr << "SDL_init Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
+    assert(SDL_Init(SDL_INIT_EVERYTHING) == 0); 
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    // FIXME: don't work with depth size = 32
-    // SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 32);
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1); 
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); 
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24); 
 
-    SDL_Window *window = SDL_CreateWindow("Empty SDL",
-            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-            win_width, win_height,
-            SDL_WINDOW_OPENGL);
-    if (window == nullptr) {
-        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
+    SDL_Window * oWindow = SDL_CreateWindow("Default", 100, 100, 320, 480, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL); 
+    assert(oWindow); 
+    SDL_GLContext oGlContext = SDL_GL_CreateContext(oWindow); 
+    assert(oGlContext); 
+    SDL_GL_MakeCurrent(oWindow, oGlContext); 
 
-    SDL_GLContext glcontext = SDL_GL_CreateContext(window);
-    int status = SDL_GL_MakeCurrent(window, glcontext);
-    if (status) {
-        std::cerr << "Failed MakeCurrent" << SDL_GetError() << std::endl;
-        return 1;
-    }
+    printf("GL_VERSION: %s\n", reinterpret_cast<const char *>(glGetString(GL_VERSION))); 
+    printf("GL_SHADING_LANGUAGE_VERSION: %s\n", reinterpret_cast<const char *>(glGetString(GL_SHADING_LANGUAGE_VERSION))); 
+
+    /* Clear our buffer with a red background */ 
+    glViewport(0, 0, 320, 480); 
+    glClearColor(1, 0, 0, 1); 
+    glClear(GL_COLOR_BUFFER_BIT); 
+    SDL_GL_SwapWindow(oWindow); 
 
     bool quit = false;
-    while (!quit) {
-        // process events
+    while(!quit) {
         SDL_Event e;
-        while(SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
+	while(SDL_PollEvent(&e)) {
+	    if (e.type == SDL_QUIT) {
+	        quit = true;
 	    }
-        }
-
-        // draw
-        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        SDL_GL_SwapWindow(window);
-
-        // delay
-        SDL_Delay(100);
+	}
+	SDL_Delay(100); 
     }
+    SDL_GL_DeleteContext(oGlContext); 
+    SDL_DestroyWindow(oWindow); 
 
-    SDL_GL_DeleteContext(glcontext);
-    SDL_Quit();
-    return 0;
-}
+    SDL_Quit(); 
+    return 0; 
+} 
